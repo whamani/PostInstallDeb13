277dcd1e1a01a6e39f21ed051bbafc6bfb16fada
Hamani Walid <whamani@proton.me>
2025-12-12 09:14:31 +0100

    PostInstallDeb13.sh

--- PostInstallDeb13.sh
+++ PostInstallDeb13.sh
@@ -38,24 +38,11 @@ netgroup:       nis" > /etc/nsswitch.conf
 #==================================================================#
 
 echo "
-# ~/.bashrc: executed by bash(1) for non-login shells.
-# Note: PS1 is set in /etc/profile, and the default umask is defined
-# in /etc/login.defs. You should not need this unless you want different
-# defaults for root.
-# PS1='${debian_chroot:+($debian_chroot)}\h:\w\$ '
-# umask 022
-
-# You may uncomment the following lines if you want `ls' to be colorized:
-#cexport LS_OPTIONS='--color=auto'
-# eval "$(dircolors)"
-# alias ls='ls $LS_OPTIONS'
-# alias ll='ls $LS_OPTIONS -l'
-# alias l='ls $LS_OPTIONS -lA'
-#
-# Some more alias to avoid making mistakes:
-# alias rm='rm -i'
-# alias cp='cp -i'
-# alias mv='mv -i'
+export LS_OPTIONS='--color=auto'
+export LS_COLORS="$(dircolors)"
+alias ls='ls $LS_OPTIONS'
+alias ll='ls $LS_OPTIONS -l'
+alias l='ls $LS_OPTIONS -lA'" >> /root/.bashrc
 
 export LS_OPTIONS='--color=auto'
 eval LS_COLORS='rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=00:su=37;41:sg=30;43:ca=00:tw=30;42:ow=34;42>
